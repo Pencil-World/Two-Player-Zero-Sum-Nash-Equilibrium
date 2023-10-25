c7a1b5d991dcaca3b3a45f98550feaeaeb250d0f
Craner <iayfnp@gmail.com>
2023-10-25 16:10:13 -0400

    Update TicTacToe

--- Two Player Zero Sum Nash Equilibrium/Reinforcement Learning.py
+++ Two Player Zero Sum Nash Equilibrium/Reinforcement Learning.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from TicTacToe import TicTacToe
 # from Connect4 import Connect4
 import datetime
@@ -6,9 +7,12 @@ from tensorflow import keras
 import numpy as np
 import random
 
+"""
+
+"""
 def Experiment():
     global gamma, test_stats
-    pi = i % 10
+    pi = epoch_count % 10
     if pi == 0:
         gamma = sum([elem[0] * elem[1] ** 5 for elem in test_stats]) / sum([elem[1] ** 5 for elem in test_stats])
         lower, upper = max(0.001, gamma - Experiment.MoE), min(gamma + Experiment.MoE, 0.999)
@@ -18,14 +22,27 @@ def Experiment():
     gamma = test_stats[pi][0]
 Experiment.MoE = 0.5
 
+"""
+
+"""
 def Conclude():
-    global HighScore
-    pi = i % 10
+    global BestScore
+    pi = epoch_count % 10
     test_stats[pi][1] = CurrScore[0]
-    if HighScore[1] < CurrScore[0]:
-        HighScore = [gamma, CurrScore[0]]
+    if BestScore[1] < CurrScore[0]:
+        BestScore = [gamma, CurrScore[0]]
         JSON = dict(zip(QTable.keys(), [repr(elem.tolist()) for elem in QTable.values()])) # works for jagged arrays. includes commas
         json.dump(JSON, open('agent.json', 'w'), indent = 4)
+
+def log(text):
+    with open('log.txt', 'a') as log:
+        log.write(text + "\n")
+
+def self_agent_action():
+    pass
+
+def other_agent_action():
+    pass
 
 def Test():
     print("\nTest")
@@ -57,76 +74,82 @@ def Test():
             state = TicTacToe()
             AgentTurn = True
 
-alpha = 0.0001
-gamma = 0.5
-episodes = 1000
-epochs = 100
-
-R = [10, -10, 0]
-test_stats = [[0.5, 1]]
-HighScore = [0, 0]
-data_size = 1_000
-
 #open('The One.json', 'w').write(open('agent.json').read())
 #Test()
 open('log.txt', 'w').close()
 
+# Variables are in alphabetical order
+alpha = 0.0001 # learning rate. model learns faster at higher alpha and learns slower at lower alpha
+depth = 2 # prediction depth. int for how many moves in the future the model considers
+episodes = 1000 # training time. model spends more time learning at higher episodes and spends less time training at lower episodes
+epochs = 100 # evolutionary time. how many models and time spent exploring those models
+gamma = 0.5 # decay rate. model considers future actions more at higher gamma and considers future actions less at lower gamma
 state = TicTacToe()
-with open('log.txt', 'a') as log:
-    log.write(f"start program\n{datetime.datetime.now()}\n")
-for i in range(data_size):
+test_stats = [[0.5, 1]]
+
+R = [10, -10, 0] # remove
+BestScore = [0, 0]
+
+log(f"starting program at {datetime.datetime.now()}")
+for epoch_count in range(epochs):
+    log(f"new model - starting epoch {epoch_count} at {datetime.datetime.now()}\n")
     Experiment()
     CurrScore = np.zeros([3])
     QTable = dict()
-    for epsilon in range(1, epochs + 1):
-        for temp in range(episodes):
-            history = []
+
+    for episode_count in range(1, episodes + 1): # {+ 1} so episode_count == episodes at last iteration and enters Conclude() block
+        history = []
+        actions = state.actions
+        AgentTurn = True;
+
+        # make this while loop a function
+        while actions.shape[0] and state.progress == "GREEN":
+            blind = True
+            values = QTable.setdefault(repr(state), np.full([9, 9], 100, dtype = np.float32)) if AgentTurn else values[action]
+            epsilon = (epsilon / epochs) * 100
+            if random.randrange(0, 100) < epsilon:
+                if AgentTurn:
+                    action =  np.array([-100 if min(elem) == 100 else min(elem) for elem in values]).argmax()
+                    blind = values[action].argmin() == 100
+                elif epsilon + 1 == epochs:
+                    # create log of best models. test it by playing against those models
+                    blind = False
+                    action = actions[random.randrange(0, actions.shape[0])] # when less than halfway through. random actions will always choose an unexplored option
+                    # when over halfway through, weight the actions appropiately
+                    # the opponent will play in the same function
+                else:
+                    action = values.argmin()
+                    blind = values[action] == 100
+
+            if blind:
+                if not AgentTurn and (9 - actions.shape[0]) < (values == 100).sum():
+                    action = actions[values[actions].argmax()]
+                else:
+                    action = actions[random.randrange(0, actions.shape[0])]
+
+            history.append((action, values[action]))
+            state.move(action, AgentTurn + 1)
             actions = state.generate()
-            AgentTurn = True;
+            AgentTurn = not AgentTurn
 
-            while actions.shape[0] and state.progress == "GREEN":
-                blind = True
-                values = QTable.setdefault(repr(state), np.full([9, 9], 100, dtype = np.float32)) if AgentTurn else values[action]
-                if random.randrange(0, 100) < epsilon * (100 // epochs):
-                    if AgentTurn:
-                        action =  np.array([-100 if min(elem) == 100 else min(elem) for elem in values]).argmax()
-                        blind = values[action].argmin() == 100
-                    elif epsilon + 1 == epochs:
-                        # create log of best models. test it by playing against those models
-                        blind = False
-                        action = actions[random.randrange(0, actions.shape[0])]
-                    else:
-                        action = values.argmin()
-                        blind = values[action] == 100
-
-                if blind:
-                    if not AgentTurn and (9 - actions.shape[0]) < (values == 100).sum():
-                        action = actions[values[actions].argmax()]
-                    else:
-                        action = actions[random.randrange(0, actions.shape[0])]
-
-                history.append((action, values[action]))
-                state.move(action, AgentTurn + 1)
-                actions = state.generate()
+        # mae this "learning" section a function as well
+        it = int(AgentTurn) if state.progress == "RED" else 2
+        if epsilon + 1 == epochs:
+            CurrScore[it] += 1
+            state = TicTacToe()
+        else:
+            reward = R[it]
+            prev = 0
+            for action, values in history[::-1]:
                 AgentTurn = not AgentTurn
+                state.move(action, 0)
+                if AgentTurn:
+                    reward *= gamma
+                    values[prev] = reward if values[prev] == 100 else values[prev] + alpha * (reward - values[prev])
+                else:
+                    prev = action
 
-            it = int(AgentTurn) if state.progress == "RED" else 2
-            if epsilon + 1 == epochs:
-                CurrScore[it] += 1
-                state = TicTacToe()
-            else:
-                reward = R[it]
-                prev = 0
-                for action, values in history[::-1]:
-                    AgentTurn = not AgentTurn
-                    state.move(action, 0)
-                    if AgentTurn:
-                        reward *= gamma
-                        values[prev] = reward if values[prev] == 100 else values[prev] + alpha * (reward - values[prev])
-                    else:
-                        prev = action
-
-    CurrScore /= episodes
-    Conclude()
-    with open('log.txt', 'a') as log:
-        log.write(f"{HighScore[0]:.3f} high score: {HighScore[1]:.3f}\t\t{gamma:.3f} win rate: {CurrScore[0]:.3f} loss rate: {CurrScore[1]:.3f} tie rate: {CurrScore[2]:.3f}\ttime: {datetime.datetime.now()}\n")
+        if not episode_count % (episodes // 10):
+            CurrScore /= episodes
+            Conclude()
+            log(f"{BestScore[0]:.3f} high score: {BestScore[1]:.3f}\t\t{gamma:.3f} win rate: {CurrScore[0]:.3f} loss rate: {CurrScore[1]:.3f} tie rate: {CurrScore[2]:.3f}\ttime: {datetime.datetime.now()}\n")
--- Two Player Zero Sum Nash Equilibrium/TicTacToe.py
+++ Two Player Zero Sum Nash Equilibrium/TicTacToe.py
@@ -1,35 +1,38 @@
 import numpy as np
 
+# reward works like this: 1 for each x, 10 for each xx, 100 for each xxx. each must have the possibility for an xxx
 class TicTacToe():
     def __init__(self, board = None):
         self.board = np.full([9], 0, dtype = np.int8)
         if board:
             for i, elem in enumerate(board):
                 self.board[i] = elem
-        self.evaluate()
 
     def __repr__(self):
         return str(self.board)
 
     def __str__(self):
         board = np.full([3, 3], "")
-        dictionary = [" ", "X", "O"]
+        dict = [" ", "X", "O"]
         for i, elem in enumerate(self.board):
-            board[i // 3][i % 3] = dictionary[elem]
+            board[i // 3][i % 3] = dict[elem]
         return str(board)
 
     def move(self, action, player):
         self.board[action] = player
-        self.evaluate()
 
-    def evaluate(self):
+    @property
+    def reward(self):
+        tokenizer = [1, 3, 9] # tokenizes the elements " ", "X", and "O" into numerical forms
+        parser = { 7: 5, 9: 10, 19: -5, 27: -10 } # converts the tokens into rewards throughout the board
+        reward = 0
         for (coord, change) in zip([0, 0, 0, 1, 2, 2, 3, 6], [1, 3, 4, 3, 2, 3, 1, 1]):
-            if 0 != self.board[coord] == self.board[coord + change] == self.board[coord + change + change]:
-                self.progress = "RED"
-                return
-        self.progress = "GREEN"
+            temp = sum(tokenizer[self.board[[coord, coord + change, coord + change + change]]])
+            reward += parser[temp]
+        return reward
 
-    def generate(self):
+    @property
+    def actions(self):
         actions = []
         for i, elem in enumerate(self.board):
             if not elem:
